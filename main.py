ccb10cb9cf535c5d69f049ef0336b6c08299bf49
Юрий <144734719+Urtets@users.noreply.github.com>
2024-08-20 18:51:39 +0300

    Indexation of strings

---
+++ main.py
@@ -0,0 +1,9 @@
+example = "How difficult it is to memorize a lot!"
+
+print(example[0])
+print(example[-1])
+half = int(len(example) / 2)
+print(example[half:])
+print(example[::-1])
+print(example[1:-1:2])
+
